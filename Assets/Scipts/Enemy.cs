e60eba73ba02dd531c2b9d4f80d6fe391a13b25b
LAPTOP-E4LRG7BI\Devashish <mannad199815@gmail.com>
2018-07-26 00:02:45 +0530

    score stuck at 0

---
+++ Assets/Scipts/Enemy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    [SerializeField] GameObject deathFX;
+    [SerializeField] Transform parent;
+
+    // Use this for initialization
+    void Start()
+    {
+        Collider boxCollider = gameObject.AddComponent<BoxCollider>();
+        boxCollider.isTrigger = false;
+    }
+
+    void OnParticleCollision(GameObject other)
+    {
+        GameObject fx = Instantiate(deathFX, transform.position, Quaternion.identity);
+        fx.transform.parent = parent;
+        Destroy(gameObject);
+    }
+}
